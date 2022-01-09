a17248d34e3de52db865035f15114dd81e80eed2
Robert-Roszak <79662439+Robert-Roszak@users.noreply.github.com>
2022-01-09 16:34:14 +0100

    Add quote testing

--- src/components/features/Quote/Quote.test.js
+++ src/components/features/Quote/Quote.test.js
@@ -4,7 +4,34 @@ import { QuoteComponent } from './Quote';
 
 describe('Component Quote', () => {
   it('should render without crashing', () => {
-    /* const component = shallow(<QuoteComponent />);
-    expect(component).toBeTruthy(); */
+    const quote = {};
+    const component = shallow(<QuoteComponent quote={quote}/>);
+    expect(component).toBeTruthy();
+  });
+
+  it('should throw error without required props', () => {
+    expect(() => shallow(<QuoteComponent />)).toThrow();
+  });
+
+  it('should render correct quote', () => {
+    const quote = {
+      quote: 'NO!',
+    };
+    const expectedQuote = quote.quote;
+    const component = shallow(<QuoteComponent quote={quote} />);
+    const renderedQuote = component.find('blockquote').text();
+    expect(renderedQuote).toEqual(expectedQuote);
+  });
+
+
+  it('should render correct character and movie', () => {
+    const quote = {
+      character: 'Frodo',
+      movie: 'Two towers',
+    };
+    const expectedRender = `- ${quote.character}, ${quote.movie}`;
+    const component = shallow(<QuoteComponent quote={quote} />);
+    const renderedText = component.find('cite').text();
+    expect(renderedText).toEqual(expectedRender);
   });
 });
